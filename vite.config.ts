260b6633713a5f0491fa5435292812c839898c6d
Héctor Zamudio <47469444+zamudio1243@users.noreply.github.com>
2026-04-02 14:27:13 -0600

    🚀 deploy netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tsconfigPaths({ projects: ["./tsconfig.json"] }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    netlify(),
   ],
-})
+});
 
-export default config
+export default config;
